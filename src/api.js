6a82ca351542f8a42fe440f0904cddf8539e003c
AyaanKola11 <kolampho6@gmail.com>
2024-01-18 15:17:46 +0200

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd8c49107f9msh5f74f30ebcd22bep1f0110jsn70c1a4256091',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "82c1d8bd6a714216ca070139b8c7b16d";
